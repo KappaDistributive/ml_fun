a3bdf250930069f7d202aeb5d70df0778ebc2b13
Stefan Mesken <kappadistributive@gmail.com>
2021-11-28 18:33:10 +0100

    fix bugs

--- src/muzero/model.py
+++ src/muzero/model.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 
 
 class AbstractMuZeroModel(ABC):
-    def __init__(self, search_depth: int):
-        self.search_depth = search_depth  # aka `K`
+    def __init__(self, num_actions: int):
+        self.num_actions = num_actions  # aka `K`
 
     @abstractmethod
     def representation_function(self, observation: tf.Tensor) -> tf.Tensor:
@@ -56,14 +56,14 @@ class AbstractMuZeroModel(ABC):
 class DenseMuZeroModel(AbstractMuZeroModel):
     def __init__(
         self,
-        search_depth: int,
+        num_actions: int,
         observation_size: int,
         action_size: int,
         state_size: int,
         hidden_layer_sizes: List[int],
         learning_rate: float = 1e-3,
     ):
-        super().__init__(search_depth)
+        super().__init__(num_actions)
         self.observation_size = observation_size
         self.action_size = action_size
         self.state_size = state_size
@@ -71,22 +71,23 @@ class DenseMuZeroModel(AbstractMuZeroModel):
         self.learning_rate = learning_rate
 
         # representation function h_{\theta}: (o^{0}) |---> (s^{0})
-        x = initial_observation = tf.keras.layers.Input(observation_size)
+        initial_observation = tf.keras.Input(observation_size)
+        x = initial_observation
         for layer_size in self.hidden_layer_sizes:
             x = tf.keras.layers.Dense(layer_size, activation="relu")(x)
-        initial_hidden_state = tf.keras.layers.Dense(self.state_size, name="s^{0}")(x)
+        initial_hidden_state = tf.keras.layers.Dense(self.state_size, name="s_0")(x)
         self.representation_model = tf.keras.Model(
             initial_observation, initial_hidden_state, name="h"
         )
 
         # dynamics function g_{\theta}: (s^{k-1}, a^{k}) |---> (r^{k}, s^{k})
-        previous_internal_state = tf.keras.layers.Input(self.state_size)
+        previous_internal_state = tf.keras.Input(self.state_size)
         action = tf.keras.layers.Input(self.action_size)
         x = tf.keras.layers.Concatenate()([previous_internal_state, action])
         for layer_size in self.hidden_layer_sizes:
             x = tf.keras.layers.Dense(layer_size, activation="relu")(x)
-        immediate_reward = tf.keras.layers.Dense(1, name="r^{k}")(x)
-        internal_state = tf.keras.layers.Dense(self.state_size, name="s^{k}")(x)
+        immediate_reward = tf.keras.layers.Dense(1, name="r_k")(x)
+        internal_state = tf.keras.layers.Dense(self.state_size, name="s_k")(x)
         self.dynamics_model = tf.keras.Model(
             [previous_internal_state, action],
             [immediate_reward, internal_state],
@@ -94,17 +95,17 @@ class DenseMuZeroModel(AbstractMuZeroModel):
         )
 
         # prediction function f_{\theta}: (s^{k})  |---> (p^{k}, v^{k})
-        x = internal_state = tf.keras.layers.Input(self.state_size)
+        x = internal_state = tf.keras.Input(self.state_size)
         for layer_size in self.hidden_layer_sizes:
             x = tf.keras.layers.Dense(layer_size, activation="relu")(x)
-        policy = tf.keras.layers.Dense(self.action_size, name="p^{k}")(x)
-        value = tf.keras.layers.Dense(1, name="v^{k}")(x)
+        policy = tf.keras.layers.Dense(self.action_size, name="p_k")(x)
+        value = tf.keras.layers.Dense(1, name="v_k")(x)
         self.prediction_model = tf.keras.Model(
             internal_state, [policy, value], name="f"
         )
 
         # mu function \mu_{\theta}: (o^{0}, a^{1}, ..., a^{K}) |---> (p^{0}, ..., p^{K}, v^{0}, ..., v^{K}, r^{1}, ..., r^{K})
-        initial_observation = tf.keras.layers.Input(self.observation_size, name="o^{0}")
+        initial_observation = tf.keras.Input(self.observation_size, name="o_0")
         previous_initial_state = self.representation_function(initial_observation)
 
         actions = []  # length K
@@ -114,22 +115,22 @@ class DenseMuZeroModel(AbstractMuZeroModel):
 
         policy, value = self.prediction_function(previous_initial_state)
         policies.append(policy)
-        values.append(values)
+        values.append(value)
 
-        for k in range(self.search_depth):
-            action = tf.keras.layer.Input(self.action_size, name=f"a_{k+1}")
+        for k in range(self.num_actions):
+            action = tf.keras.Input(self.action_size, name=f"a_{k+1}")
             actions.append(action)
             immediate_reward, internal_state = self.dynamics_model(
-                previous_initial_state, action
+                [previous_initial_state, action]
             )
             policy, value = self.prediction_function(internal_state)
             policies.append(policy)
-            values.append(values)
-            rewards.append(rewards)
+            values.append(value)
+            rewards.append(immediate_reward)
 
             previous_initial_state = initial_hidden_state
 
-        self.mu_model = tf.keras.model(
+        self.mu_model = tf.keras.Model(
             [initial_observation] + actions, policies + values + rewards, name="mu"
         )
         losses: List[str] = (
