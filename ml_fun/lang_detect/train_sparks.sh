4b10ac2873fc38f3f63a8168fefd61792a39d30a
Stefan Mesken <kappadistributive@gmail.com>
2026-05-11 21:57:08 +0200

    [lang_detect] train distributed

--- ml_fun/lang_detect/train_sparks.sh
+++ ml_fun/lang_detect/train_sparks.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+set -ex
 uv run torchrun --nnodes=2 --nproc_per_node=1 --node_rank=$([ "$(hostname)" = "pinky" ] && echo 0 || echo 1) --master_addr=169.254.239.145 --master_port=29500 main.py
